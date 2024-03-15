8a2792e6ab2aa1f71ceb06f623aa468e9c0d3556
juyeongnoh <juyeongnoh@gmail.com>
2024-03-16 03:04:04 +0900

    feat: DropdownMBTI 컴포넌트 구현
    MBTI 드롭다운 구현과 함께 `useDetectClose` 커스텀 훅을 구현했습니다. 컴포넌트 바깥을 클릭하면 컴포넌트의 상태가 변하는 기능을 수행합니다. (ex. 바깥을 클릭하면 드롭다운 닫힘)
    
    resolves: #10

---
+++ src/components/register/DropdownMBTI.jsx
@@ -0,0 +1,101 @@
+import React, { useState, useRef } from "react";
+import styled, { keyframes } from "styled-components";
+import useDetectClose from "../../hooks/useDetectClose";
+
+const DropdownMBTI = () => {
+  const [selected, setSelected] = useState("");
+  const dropdownRef = useRef(null);
+  const [isOpen, setIsOpen] = useDetectClose(dropdownRef, false);
+
+  return (
+    <DropdownContainer ref={dropdownRef}>
+      <DropdownButton onClick={() => setIsOpen(!isOpen)}>
+        {selected || "MBTI를 선택해주세요."}
+      </DropdownButton>
+      {isOpen && (
+        <DropdownContent>
+          <Placeholder>MBTI를 선택해주세요.</Placeholder>
+          {mbtiTypes.map((type) => (
+            <DropdownItem
+              key={type}
+              onClick={() => {
+                setSelected(type);
+                setIsOpen(false);
+              }}>
+              {type}
+            </DropdownItem>
+          ))}
+        </DropdownContent>
+      )}
+    </DropdownContainer>
+  );
+};
+
+const DropdownContainer = styled.div`
+  position: relative;
+`;
+
+const DropdownButton = styled.div`
+  padding: 0.75rem 1.25rem;
+  background-color: #ffffff;
+  border: 1px solid #d9d9d9;
+  border-radius: 8px;
+`;
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const DropdownContent = styled.div`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 256px;
+  overflow: auto;
+  border-radius: 8px;
+  background-color: #ffffff;
+  border: 1px solid #ddd;
+  z-index: 1;
+  animation: ${fadeIn} 0.2s ease-out;
+`;
+
+const Placeholder = styled.div`
+  background-color: #ff625d;
+  color: #fff;
+  padding: 0.75rem 1.25rem;
+`;
+
+const DropdownItem = styled.div`
+  padding: 0.75rem 1.25rem;
+  &:hover {
+    background-color: #f1f1f1;
+  }
+`;
+
+const mbtiTypes = [
+  "ISTJ",
+  "ISFJ",
+  "INFJ",
+  "INTJ",
+  "ISTP",
+  "ISFP",
+  "INFP",
+  "INTP",
+  "ESTP",
+  "ESFP",
+  "ENFP",
+  "ENTP",
+  "ESTJ",
+  "ESFJ",
+  "ENFJ",
+  "ENTJ",
+];
+
+export default DropdownMBTI;
