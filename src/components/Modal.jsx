321b78b1ac78daf9cce51c93ab985f61f6bf8f3b
Solmi <7548ck@naver.com>
2024-03-16 02:03:29 +0900

    feat: Modal 컴포넌트 구현
    resolves: #11

---
+++ src/components/Modal.jsx
@@ -0,0 +1,74 @@
+import styled from 'styled-components'
+import { useRef, forwardRef, useImperativeHandle } from 'react';
+import Button from './Button';
+
+import CancelButton from '../assets/cancel-button.png';
+
+const StyledDialog = styled.dialog`
+  border: none;
+  border-radius: 8px;
+  padding: 20px;
+  box-shadow: 0px 4px 10px 10px #3333334D;
+  position: relative;
+`;
+
+const CloseButton = styled.button`
+  flex: 1;
+  background: none;
+  border: none;
+
+  img {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 10px;
+  }
+`;
+
+const WrapContent = styled.div`
+  display: flex;
+`;
+
+const Modal = forwardRef(({ modalId, content, buttonLabel }, ref) => {
+
+  const dialog = useRef();
+  const handleCloseModal = () => {
+    dialog.current.close();
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+      close() {
+        dialog.current.close();
+      }
+    };
+  });
+
+  return (
+    <>
+      <StyledDialog ref={dialog}>
+        <div>
+          <CloseButton type="button" onClick={handleCloseModal}>
+            <img src={CancelButton} alt="Close" />
+          </CloseButton>
+          <WrapContent>
+            {content}
+          </WrapContent>
+          <Button
+            bgColor={"coral"}
+            textColor={"white"}
+            size={"medium"}
+            form={modalId}
+            onClick={handleCloseModal}>
+            {buttonLabel}
+          </Button>
+        </div>
+      </StyledDialog>
+    </>
+  )
+})
+
+export default Modal;
