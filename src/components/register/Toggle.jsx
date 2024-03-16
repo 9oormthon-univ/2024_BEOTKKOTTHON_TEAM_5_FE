99ba666ab66536138f63cdbb4f851e191e3f952f
juyeongnoh <juyeongnoh@gmail.com>
2024-03-16 16:40:50 +0900

    feat: RegisterPage 구현
    - 정규표현식 검사
    - Recoil state에 input value 저장
    
    resolves: #34

--- src/components/register/Toggle.jsx
+++ src/components/register/Toggle.jsx
@@ -1,34 +1,55 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const ToggleContainer = styled.div`
   display: flex;
-  border: 1px solid #D9D9D9;
+  border: 1px solid #d9d9d9;
 `;
 
 const ToggleOption = styled.div`
   flex: 1;
   padding: 10px 20px;
   text-align: center;
-  background-color: ${({ isActive }) => (isActive ? '#FF625D' : '#FBFBFB')};
-  color: ${({ isActive }) => (isActive ? 'white' : 'black')};
+  background-color: ${({ isActive }) => (isActive ? "#FF625D" : "#FBFBFB")};
+  color: ${({ isActive }) => (isActive ? "white" : "black")};
   transition: background-color 0.3s, color 0.3s;
 `;
 
-const ToggleSwitch = ({ options = ['남', '여'] }) => {
+const Label = styled.label`
+  display: block;
+  font-weight: 700;
+  color: #333333;
+  margin-bottom: 1rem;
+`;
+
+const ToggleSwitch = ({
+  label,
+  options = ["남", "여"],
+  registerData,
+  setRegisterData,
+}) => {
   const [activeIndex, setActiveIndex] = useState(0);
+
   return (
-    <ToggleContainer>
-      {options.map((option, index) => (
-        <ToggleOption
-          key={index}
-          isActive={index === activeIndex}
-          onClick={() => setActiveIndex(index)}
-        >
-          {option}
-        </ToggleOption>
-      ))}
-    </ToggleContainer>
+    <div>
+      <Label>{label}</Label>
+      <ToggleContainer>
+        {options.map((option, index) => (
+          <ToggleOption
+            key={index}
+            isActive={index === activeIndex}
+            onClick={() => {
+              setActiveIndex(index);
+              setRegisterData({
+                ...registerData,
+                ["gender"]: activeIndex === 1 ? "M" : "F",
+              });
+            }}>
+            {option}
+          </ToggleOption>
+        ))}
+      </ToggleContainer>
+    </div>
   );
 };
 
