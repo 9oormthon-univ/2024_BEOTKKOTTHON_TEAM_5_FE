bc3596d11a57dcc5e5beaa9e4c81b9bad9fdbfec
juyeongnoh <juyeongnoh@gmail.com>
2024-03-16 14:52:28 +0900

    feat: 랜딩 페이지 구현
    resolves: #13

--- src/pages/root/LandingPage.jsx
+++ src/pages/root/LandingPage.jsx
@@ -1,17 +1,61 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <div>distance</div>
-      <div>
-        <button>학생 메일로 가입하기</button>
-      </div>
-      <div>
-        <button>로그인</button>
-      </div>
-    </div>
+    <Background>
+      <WrapContent>
+        <img src="/assets/logo.svg" alt="디스턴스 로고" />
+        <WrapButton>
+          <LongButton
+            onClick={() => {
+              navigate("/register/user");
+            }}>
+            학생 메일로 가입하기
+          </LongButton>
+          <StyledLink to="">로그인</StyledLink>
+        </WrapButton>
+      </WrapContent>
+    </Background>
   );
 };
+
+const Background = styled.div`
+  width: 100dvw;
+  height: 100dvh;
+  background-color: #ff625d;
+`;
+
+const WrapContent = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const WrapButton = styled.div`
+  display: grid;
+  gap: 1rem;
+  text-align: center;
+  position: absolute;
+  bottom: 2rem;
+`;
+
+const LongButton = styled.div`
+  background-color: #ffac0b;
+  font-weight: 600;
+  color: #333333;
+  border: none;
+  padding: 1.5rem 6.5rem;
+  border-radius: 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  color: #ffffff;
+`;
 
 export default LandingPage;
