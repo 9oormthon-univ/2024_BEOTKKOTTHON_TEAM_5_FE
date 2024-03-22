f20f01335a923176d703a0e138509c7be9e5ae4a
Solmi <7548ck@naver.com>
2024-03-23 02:53:35 +0900

    feat: 홈 화면에 채팅input 삭제

--- src/pages/home/HomeIndexPage.jsx
+++ src/pages/home/HomeIndexPage.jsx
@@ -10,17 +10,7 @@ import Modal from "../../components/common/Modal";
 import { useNavigate } from "react-router-dom";
 
 const HomeIndexPage = () => {
-  const [chatroomCreate, setChatroomCreate] = useState({
-    memberId: "",
-    roomName: "",
-  });
-
-  const [chatroomIdToEnter, setChatroomIdToEnter] = useState({
-    myId: "",
-    opponentId: "",
-    chatRoomId: "",
-  });
-
+  
   const profileModal = useRef();
   const [selectedProfile, setSelectedProfile] = useState();
   const navigate = useNavigate();
@@ -61,27 +51,6 @@ const HomeIndexPage = () => {
       )
     );
   };
-
-  const handleChangeChatroom = (e) => {
-    setChatroomCreate({
-      ...chatroomCreate,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleChangeChatroomId = (e) => {
-    setChatroomIdToEnter({
-      ...chatroomIdToEnter,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const chatroomDisabled = chatroomCreate.memberId === "";
-
-  const chatroomIdDisabled =
-    chatroomIdToEnter.myId === "" ||
-    chatroomIdToEnter.opponentId === "" ||
-    chatroomIdToEnter.chatRoomId === "";
 
   useEffect(() => {
     console.log(memberState);
@@ -170,28 +139,6 @@ const HomeIndexPage = () => {
     profileModal.current.close();
   };
 
-  /**
-   * TEMP AREA
-   */
-  const createChatroom = async () => {
-    await authInstance
-      .post("/chatroom/create", {
-        memberId: chatroomCreate.memberId,
-      })
-      .then((res) => alert(`${res.data}번 방이 성공적으로 생성되었습니다!`))
-      .catch((error) => console.log(error));
-  };
-
-  const navigateChatroom = () => {
-    navigate(`/chat/${chatroomIdToEnter.chatRoomId}`, {
-      state: {
-        myId: chatroomIdToEnter.myId,
-        opponentId: chatroomIdToEnter.opponentId,
-        roomId: chatroomIdToEnter.chatRoomId,
-      },
-    });
-  };
-
   return (
     <>
       <Modal
@@ -204,63 +151,6 @@ const HomeIndexPage = () => {
       />
       <HomeContainer>
         <Header />
-        <div>
-          <div style={{ fontWeight: "900" }}>/api/chatroom/create</div>
-          <label htmlFor="memberId">memberId</label>
-          <input
-            style={{ width: "40px" }}
-            id="memberId"
-            name="memberId"
-            onChange={handleChangeChatroom}
-            type="number"
-          />
-          <label htmlFor="roomName">roomName</label>
-          <input
-            style={{ width: "40px" }}
-            id="roomName"
-            name="roomName"
-            onChange={handleChangeChatroom}
-            type="text"
-          />
-          <button onClick={createChatroom} disabled={chatroomDisabled}>
-            채팅방 생성
-          </button>
-        </div>
-
-        <br />
-
-        <div style={{ fontWeight: "900" }}>채팅방 입장하기</div>
-        <label htmlFor="myId">내ID</label>
-        <input
-          style={{ width: "40px" }}
-          id="myId"
-          name="myId"
-          onChange={handleChangeChatroomId}
-          type="number"
-        />
-        <label htmlFor="opponentId">상대ID</label>
-        <input
-          style={{ width: "40px" }}
-          id="opponentId"
-          name="opponentId"
-          onChange={handleChangeChatroomId}
-          type="number"
-        />
-        <label htmlFor="chatRoomId">chatRoomId</label>
-        <input
-          style={{ width: "40px" }}
-          id="chatRoomId"
-          name="chatRoomId"
-          onChange={handleChangeChatroomId}
-          type="number"
-        />
-        <button onClick={navigateChatroom} disabled={chatroomIdDisabled}>
-          {chatroomIdToEnter.chatRoomId && chatroomIdToEnter.chatRoomId + "번"}{" "}
-          채팅방 입장
-        </button>
-
-        <br />
-
         <ProfileContainer>
           {memberState.map((profile, index) => (
             <Profile
