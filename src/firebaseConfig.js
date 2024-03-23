91ac7db1cf746d2cf03f5624b2c795b680fbac67
Solmi <7548ck@naver.com>
2024-03-24 02:28:15 +0900

    feat: 포그라운드 메세지 수신

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -18,6 +18,7 @@ const messaging = getMessaging(FBapp);
 
 // client 토큰 발급 받기
 export const onGetToken = async () => {
+  console.log(messaging)
   return getToken(messaging, {
     vapidKey:
       "BL3TJNMH7nqPV4wLrzYhZOzefyN-WlP5--CzS1RO2WWKACm5b32tv2caLiKdbahJBQDeDpsNwZbGvrZJjajR26E",
@@ -34,8 +35,25 @@ export const onGetToken = async () => {
     });
 };
 
+Notification.requestPermission().then(permission => {
+  if (permission === "granted") {
+    console.log("알림 권한이 허용되었습니다.");
+  } else {
+    console.log("알림 권한이 거부되었습니다.");
+  }
+});
+
 //포그라운드 메시지 수신
 onMessage(messaging, (payload) => {
   console.log("Message received. ", payload);
-  // ...
+  // 알림 권한이 허용되었다면, 사용자에게 알림 표시
+  if (Notification.permission === "granted") {
+    const notificationTitle = payload.notification.title; // 메시지에서 제목 추출
+    const notificationOptions = {
+      body: payload.notification.body, // 메시지에서 본문 추출
+      icon: payload.notification.icon, // 메시지에서 아이콘 URL 추출 (선택 사항)
+    };
+
+    new Notification(notificationTitle, notificationOptions);
+  }
 });
